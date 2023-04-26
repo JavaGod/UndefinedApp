a0b2749812276b1c75800c39d48f9ffc3748eeae
MonkiFantasy <Monki4536251@outlook.com>
2023-04-26 18:50:34 +0800

    修改连接校园网的模式从打开网页到Http访问 增加了app网络访问权限 增加android:usesCleartextTraffic="true"

--- app/src/main/java/monki/study/undefinedapp/HomeActivity.java
+++ app/src/main/java/monki/study/undefinedapp/HomeActivity.java
@@ -5,8 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.NoRouteToHostException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -31,10 +45,69 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
 
         } else if (v.getId() == R.id.bt_Login) {
-            Uri content_url = Uri.parse("http://10.255.0.19/drcom/login?callback=dr1003&DDDDD=2021304400@cmcc&upass=0128501&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&v=7759");
+            /*Uri content_url = Uri.parse("http://10.255.0.19/drcom/login?callback=dr1003&DDDDD=2021304400@cmcc&upass=0128501&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&v=7759");
             //Uri content_url = Uri.parse("http://10.255.0.19");
             intent.setData(content_url);
-            startActivity(intent);
+            startActivity(intent);*/
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Looper.prepare();
+                        Toast.makeText(HomeActivity.this,"开始建立连接...",Toast.LENGTH_LONG).show();
+                        URL url = new URL("http://10.255.0.19/drcom/login?callback=dr1003&DDDDD=2021304400@cmcc&upass=0128501&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&v=7759"); // 替换成要打开的网址
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setRequestMethod("GET");
+                        InputStream inputStream = conn.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        Toast.makeText(HomeActivity.this,"连接建立成功...开始读取数据",Toast.LENGTH_SHORT).show();
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        reader.close();
+                        conn.disconnect();
+                        Toast.makeText(HomeActivity.this,"读取数据成功",Toast.LENGTH_SHORT).show();
+                        Log.d("Monki","读取数据成功");
+                        String content = response.toString();
+                        Pattern pattern = Pattern.compile("\"result\":(\\d+)");
+                        Matcher matcher = pattern.matcher(content);
+                        Toast.makeText(HomeActivity.this,"开始检测连通状态..." ,Toast.LENGTH_SHORT).show();
+                        Log.d("Monki","开始检测连通状态...");
+                        if (matcher.find()) {
+                            String result = matcher.group(0);
+                            Log.d("Monki","result="+result+"\"");
+                            //runOnUiThread(new Runnable() {
+                            //@Override
+                            // public void run() {
+                            if(result.equals("\"result\":1"))
+                            {
+                                Toast.makeText(HomeActivity.this,"自动连接成功",Toast.LENGTH_SHORT).show();
+                                Log.d("Monki","自动连接成功");
+                            }
+                            else if(result.equals("\"result\":0")){
+                                Toast.makeText(HomeActivity.this,"自动连接失败，请重新连接",Toast.LENGTH_SHORT).show();
+                                Log.d("Monki","自动连接失败，请重新连接");
+                            }
+                            // }
+                            //});
+
+                        }else{
+                            Toast.makeText(HomeActivity.this,"未获取到信息，请稍后重试",Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (ConnectException ce){
+                        Toast.makeText(HomeActivity.this,"连接出错，请检查你的网络",Toast.LENGTH_SHORT).show();
+                    }catch (NoRouteToHostException ne){
+                        Toast.makeText(HomeActivity.this,"连接出错，请检查你的网络",Toast.LENGTH_SHORT).show();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Looper.loop();
+                }
+            }).start();
         }
     }
 }
